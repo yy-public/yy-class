4fff44653f71afcdb8812df48ced052aa38a5172
yy-public <2421035899@qq.com>
2019-12-05 22:59:37 +0800

    普通的练习

---
+++ day12/Demo01Practice.java
@@ -0,0 +1,19 @@
+package day12;
+/*
+有一个400米一圈的操场，一个人要跑10000米，
+第一圈50秒，其后每一圈都比前一圈慢1秒，
+按照这个规则计算跑完10000米需要多少秒
+ */
+public class Demo01Practice {
+    public static void main(String[] args) {
+        int round=10000/400;
+        int sum=0;
+        int time=50;
+        for(int i=1;i<=round;i++) {
+            sum+=time;
+            System.out.println("圈数:"+i+",时间:"+time+",花的时间:"+sum);
+            time++;
+        }
+        System.out.println(sum);
+    }
+}
