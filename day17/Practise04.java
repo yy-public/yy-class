63d393ee7e68ee148db9c16a7ec23bf2086c97ac
yy-public <2421035899@qq.com>
2019-12-24 22:59:25 +0800

    Practise04

---
+++ day17/Practise04.java
@@ -0,0 +1,24 @@
+package day17;
+/*
+井里有一只蜗牛，他白天往上爬5米，晚上掉3.5米，井深56.7米
+计算蜗牛需要多少天才能从井底到爬出来
+ */
+public class Practise04 {
+    public static void main(String[] args) {
+        int day=1;//天数
+        double sum=0;//爬过的距离
+        while(true) {
+            //白天向上爬5米
+            sum+=5;
+            System.out.println("day:"+day+",sum:"+sum);
+            if(sum>=56.7) {//如果爬出了井
+                //退出循环
+                break;
+            }
+            //晚上掉3.5;
+            sum-=3.5;
+            day++;
+        }
+        System.out.println(day);
+    }
+}
